286fee48b434b8ebc7569caa92c46701b4e1ad69
Sebastiaan ten Pas <sebastiaan@diggimedia.nl>
2017-09-29 19:53:24 +0200

    Fix WSGI Whitenoise static serving

--- project_name/project_name/wsgi.py
+++ project_name/project_name/wsgi.py
@@ -9,5 +9,7 @@ https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
 from {{ project_name }}.settings import set_settings_module
 set_settings_module()
 
+from whitenoise.django import DjangoWhiteNoise
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
+application = DjangoWhiteNoise(application)
